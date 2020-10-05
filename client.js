f0f95f5abafe4213d0d9e4ec7d051da436441d07
Kelly Danger <kellyanndanger@gmail.com>
2020-10-05 17:19:34 -0500

    messages working for correct and incorrect clicks

--- client.js
+++ client.js
@@ -35,7 +35,11 @@ function promptPlayer(){
 //if the player clicks on the correct player
 function clickOnPic(){
   let clickedPicName = $(this).data('picname');
-  if(clickedPicName === $('#dispName').text())
+  if(clickedPicName === $('#dispName').text()){
     alert(`Correct! That IS ${clickedPicName}'s pic!`)
-}
+  }//end if
+  else {
+    alert(`Sorry! That's not ${$('#dispName').text()}`)
+  }
+}//end clickOnPic 
 //give them a success message
