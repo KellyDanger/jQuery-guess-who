a0702076df78f05e2b589ba95ef0a7b7fbd4371f
Kelly Danger <kellyanndanger@gmail.com>
2020-10-05 16:26:57 -0500

    new divs appending correctly

--- client.js
+++ client.js
@@ -9,7 +9,15 @@ function onReady(){
 function appendPeople(){
   console.log('people');
   //loop through the people array 
+  for(let person of people){
   //append a div for each person in the array
+    $('body').append(`
+      <div>
+        This is a new div
+      </div>
+    `)
+  }
+
   //div should contain an image from each gitHub profile
   
 }//end appendPeople
